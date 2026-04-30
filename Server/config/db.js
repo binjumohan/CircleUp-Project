37cb3e3b53c28cbae2d4354f83c20edcf24a7e23
Binju Mohan <you@example.com>
2026-04-30 22:03:56 +0530

    connection error fixed

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    if (isConnected) return;
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = conn.connections[0].readyState;
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.error("DB ERROR:", error.message);
+
   }
 };
 
